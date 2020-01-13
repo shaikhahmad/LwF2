bff2a5fd461b3a6061d59620dd36dedad71c8cfd
Muhammad Ahmad <45653974+shaikhahmad@users.noreply.github.com>
2020-01-13 12:00:35 +0500

    first upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from LwF import LwFmodel
+from ResNet import resnet18_cbam
+parser=10
+numclass=20 #int(100/parser)
+task_size=5 #int(100/parser)
+feature_extractor=resnet18_cbam()
+img_size=32
+batch_size=128
+task_size=int(100/parser)
+memory_size=2000
+epochs=2
+learning_rate=2.0
+
+model=LwFmodel(numclass,feature_extractor,batch_size,epochs,learning_rate,task_size)
+
+for i in range(10):
+    model.beforeTrain()
+    accuracy=model.train()
+    model.afterTrain(accuracy)
