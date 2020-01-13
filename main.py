a9dd7ea8abf303065b4d033c9dba14793decf0e8
Muhammad Ahmad <muhammad.ahmad@arpatech.com>
2020-01-13 12:05:13 +0500

    2nd

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from LwF import LwFmodel
 from ResNet import resnet18_cbam
 parser=10
-numclass=20 #int(100/parser)
-task_size=5 #int(100/parser)
+numclass=int(100/parser)
+task_size=int(100/parser)
 feature_extractor=resnet18_cbam()
 img_size=32
 batch_size=128
 task_size=int(100/parser)
 memory_size=2000
-epochs=2
+epochs=100
 learning_rate=2.0
 
 model=LwFmodel(numclass,feature_extractor,batch_size,epochs,learning_rate,task_size)
