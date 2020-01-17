f39dfa0b8d39d62164832bc7081aa99f733be201
Muhammad Ahmad <muhammad.ahmad@arpatech.com>
2020-01-17 16:49:57 +0500

    LWF Changes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from LwF import LwFmodel
 from ResNet import resnet18_cbam
-parser=5
-numclass=int(10/parser)
-task_size=int(10/parser)
+parser=2
+numclass=int(20/parser)
+task_size=int(20/parser)
 feature_extractor=resnet18_cbam()
 img_size=32
 batch_size=200
@@ -31,10 +31,13 @@ normal_transforms = {
 svhn = {x: datasets.SVHN(root='SVHN', split=x if x == 'train' else 'test',
                                         download=True, transform=normal_transforms[x])
          for x in ['train', 'val']}
+cifar = {x: myDatasets.CIFAR10(root='CIFAR10', extend =10, train=True if x == 'train' else False,
+                                        download=True, transform=normal_transforms[x])
+         for x in ['train', 'val']}
 svhn_loader = {x: torch.utils.data.DataLoader(svhn[x], batch_size=batch_size,
                                                shuffle=True, num_workers=4)
                 for x in ['train', 'val']}
-jointdata  = {x: myDatasets.ConcatDataset(svhn[x]) for x in ['train', 'val']}
+jointdata  = {x: myDatasets.ConcatDataset(svhn[x], cifar[x]) for x in ['train', 'val']}
 
 model=LwFmodel(jointdata, numclass,feature_extractor,batch_size,epochs,learning_rate,task_size)
 
