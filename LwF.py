2d32a319c892a6c111fac3c55e8e32848f378d96
Muhammad Ahmad <ahmad6346@yahoo.com>
2020-01-29 17:13:10 +0500

    LWF

--- LwF.py
+++ LwF.py
@@ -98,7 +98,7 @@ class LwFmodel:
                 opt = optim.SGD(self.model.parameters(), lr=self.learning_rate / 125, momentum=0.9, nesterov=True,
                                 weight_decay=0.00001)
                 print("change learning rate%.5f" % (self.learning_rate / 125))
-            for step, (images, target) in enumerate(tqdm(self.train_loader)):
+            for step, (images, target) in enumerate(self.train_loader):
                 images = images.float()
                 images, target = images.to(device), target.to(device)
                 opt.zero_grad()
